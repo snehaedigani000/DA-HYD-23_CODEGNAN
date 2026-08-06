9747e4dfe2d866c7d4f00bda5009609ee4d00a63
snehaedigani000 <snehaedigani.0@gmail.com>
2026-08-06 13:01:10 +0530

    task3

---
+++ task2.py
@@ -0,0 +1,7 @@
+#task
+alphabet = ""
+
+for i in range(ord('A'), ord('Z') + 1):
+    alphabet += chr(i)
+
+print(alphabet)
---
+++ task3.py
@@ -0,0 +1,42 @@
+'''#task
+products=list(map(int,input("Enter the prices:").split(',')))
+total=0
+for i in products:
+         total=total+i
+print(total)'''
+
+
+'''#print a python program  on password analyzer to count the upper lower special characters
+password = input("Enter a password: ")
+uppercase = 0
+lowercase = 0
+digits = 0
+special = 0
+for ch in password:
+    if ch.isupper():
+        uppercase += 1
+    elif ch.islower():
+        lowercase += 1
+    elif ch.isdigit():
+        digits += 1
+    else:
+        special += 1
+
+print("\nPassword Analysis")
+print("-----------------")
+print("Uppercase letters are:", uppercase)
+print("Lowercase letters are:", lowercase)
+print("Digits letters are:", digits)
+print("Special characters are:", special)
+'''
+'''#task
+email = input("Enter gmail:").split()
+for mail in email:
+    print(mail.split{"@"}[1])
+'''
+
+ 
+
+
+    
+    
